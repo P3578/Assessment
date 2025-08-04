eeb96128af482a6956640b1bd993889e8b312cb7
P3578 <parthpatel089.pp@gmail.com>
2025-08-04 12:35:16 +0530

    Delete snippets_app directory

--- snippets_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('snippets_list/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
-    path('comments/', views.comment_list),
-]
